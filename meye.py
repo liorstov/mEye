e576246b7fae24e2a73bfd7e6b648bc7a72cb66a
liorstov <liorstov@gmail.com>
2020-12-14 19:52:49 +0200

    correction

--- meye.py
+++ meye.py
@@ -16,8 +16,6 @@ maxFrames = 50
 numPixels = 60
 focallength = 800
 dt = 1
-
-"""get the input"""
 world = pn.read_csv("C:\\Users\\Lior\\SandBox\\untitled\\only_target\\p.csv")
 p = pn.read_csv("C:\\Users\\Lior\\SandBox\\untitled\\only_target\\pp.csv")
 Conversion = pn.read_csv("C:\\Users\\Lior\\SandBox\\untitled\\only_target\\conversion_matrices.csv")
@@ -35,12 +33,10 @@ def estimateDistance():
     return cameraImage.item(2)
 
 def convertWorldToEgo(frame,pixel):
-    """convert real wold to ego0"""
     worldPixel = world.loc[(world['Frame']==frame) & (world['Pixel']==pixel), ['X', 'Y', 'Z', 'home']].to_numpy().reshape(4, 1)
     return (T_e_e0[frame].reshape(4, 4))[:3]*worldPixel
 
 def  convertWorldToEgoToTarget(frame,pixel):
-    """convert world to target"""
     worldPixel = world.loc[(world['Frame']==frame) & (world['Pixel']==pixel), ['X', 'Y', 'Z', 'home']].to_numpy().reshape(4, 1)
     return (T_t_e[frame].reshape(4,4)*T_e_e0[frame].reshape(4, 4))[:3]*worldPixel
 
@@ -63,6 +59,7 @@ def homogenize(frame,pixel):
     cameraPixel = p.loc[(p['Frame']==frame) & (p['Pixel']==pixel),['U','V']].to_numpy()
     U = cameraPixel[0,0]
     V = cameraPixel[0,1]
+
     """get Y coordinate from world first frame assuming dy=0"""
     Y = convertWorldToCamera(frame,pixel)[1,0]
 
@@ -129,13 +126,13 @@ def ConvertCameraToEgo0():
         frame = row['Frame']
         CameraPixel = homogenize(frame,pixel)
         CameraPixel = np.hstack([CameraPixel, 1])
-        x,y,z = np.squeeze(np.array(T_e_e0[frame].reshape(4,4)[:3] *CameraPixel.reshape(4,1)))
+        x,y,z = np.squeeze(np.array(np.linalg.inv(T_e_e0[frame].reshape(4,4))[:3] *CameraPixel.reshape(4,1)))
         data = data.append({"Pixel": pixel, "X": x, "Y": y, "Z": z,"Frame": frame}, True)
     return data
 
 def calculateCentroid(frame,converted):
     """calculate center of gravity"""
-    return (converted.loc[converted['Frame']==frame].sum()/numPixels).to_numpy()[2:5]
+    return np.mean(converted.loc[converted['Frame']==frame])[['X','Y','Z']].to_numpy()
 
 def calculateHeading(frame, converted,carLength):
     if frame == 0 or frame>=maxFrames:
@@ -143,35 +140,39 @@ def calculateHeading(frame, converted,carLength):
 
     difference = getDiffConvertedFrames(frame-1,frame,converted);
 
+
+
+    """get front and rear points, assuming front has biggest dx"""
+    front = difference.loc[abs(difference["dx"]).idxmax()]['pixel']
+    rear = difference.loc[abs(difference["dx"]).idxmin()]['pixel']
+
     """check if bycicle model or simple motion with fix heading"""
     if (difference[["dx","dz"]].round(3).nunique()==1).all():
         """if fix heading return 0"""
         return 0
-    else:
-        """get front and rear points, assuming front has biggest dx"""
-        front = difference.loc[abs(difference["dx"]).idxmax()]['pixel']
-        rear = difference.loc[abs(difference["dx"]).idxmin()]['pixel']
 
-        """calculate centroid"""
+    """get vector of plane and normalise"""
+    surface = getConvertedFramePixel(frame, front,converted) - getConvertedFramePixel(frame, rear, converted)
 
-        """get vector of plane and normalise"""
-        surface = getConvertedFramePixel(frame, front,converted) - getConvertedFramePixel(frame, rear, converted)
+    """get vector of steer"""
+    steer = getConvertedFramePixel(frame, front,converted) - getConvertedFramePixel(frame - 1, front,converted)
 
-        """get vector of steer"""
-        steer = getConvertedFramePixel(frame, front,converted) - getConvertedFramePixel(frame - 1, front,converted)
+    """calculate delta while looking down in y axes"""
+    delta = vg.signed_angle(surface,steer,np.array([0,1,0]), units="rad")
 
-        """calculate delta while looking down in y axes"""
-        delta = vg.signed_angle(surface,steer,np.array([0,1,0]), units="rad")
+    """if steer angle is big it can also mean fix heading"""
+    if (np.degrees(delta) > 80):
+        """if fix heading return 0"""
+        return 0
+    """calculate velocity vector at rear assuming dt =1 s"""
+    velo = math.sqrt(difference.loc[difference['pixel']==rear]['dz']**2+difference.loc[difference['pixel']==rear]['dx']**2) / dt
 
-        """calculate velocity vector at rear assuming dt =1 s"""
-        velo = math.sqrt(difference.loc[difference['pixel']==rear]['dz']**2+difference.loc[difference['pixel']==rear]['dx']**2) / dt
-
-        """vehicle length"""
+    """vehicle length"""
 
 
-        """calculate heading according to bicycle model"""
-        R = carLength/math.tan(delta)
-        HeadingChange = velo/R
+    """calculate heading according to bicycle model"""
+    R = carLength/math.tan(delta)
+    HeadingChange = velo/R
 
     return math.degrees(HeadingChange)
 
@@ -188,20 +189,13 @@ def TransformTwoCentroids(pointsCloud):
         Centroids = Centroids.append({"Pixel": 2, "X": cent2[0], "Y": cent2[1], "Z": cent2[2], "Frame": frame}, True)
     return Centroids
 
-def printDots(data,frames):
-    a = Centroids.loc[(Centroids['Frame']) <50]
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(a['X'], a['Y'], a['Z'], c=a['Frame'])
-    ax.scatter3D(a['X'], a['Y'], a['Z'], color = 'red')
-    ax.invert_xaxis()
-    plt.xlim(-4,4)
-    ax.cla()
 
 def convertPixelCS(CS,frame,pixel):
     pixel = np.hstack([pixel, 1])
     return np.squeeze(np.array((CS[frame].reshape(4, 4))[:3] * pixel.reshape(4, 1)))
 
 def lineraRegressionFrame(frame,converted):
+    """ This function estimates the dispersion of a frame using R2 score. Trearing the pixel cloud on (X,Z) plane."""
     group = converted.loc[converted["Frame"] == frame][["X", "Z"]].__array__()
     stat = stats.linregress(group[:, 0], group[:, 1])
     return stat.rvalue
@@ -212,7 +206,7 @@ def FurthestPointsFrame(frame,data):
     return np.max(distances)
 
 def calculatYawFromWorld(frame):
-    """calculate furthest points to estimate target surface"""
+    """calculate Yaw"""
     distances = squareform(pdist(world.loc[world["Frame"] == frame].to_numpy()[:, 1:4]))
     points = np.where(distances==np.max(distances))
     surface = getWorldFramePixel(frame, points[0][1])-getWorldFramePixel(frame, points[0][0])
@@ -222,50 +216,87 @@ def calculatYawFromWorld(frame):
 def mainFunction():
     """get first frame and pixel from world to compute initial yaw"""
     yaw = calculatYawFromWorld(0)
+
+    """calculate car length from first world frame (given)"""
     carLength = FurthestPointsFrame(0,world)
+
+    """get given camera frames and create centroids"""
     convertedEgo0 = ConvertCameraToEgo0()
     Centroids = TransformTwoCentroids(convertedEgo0)
 
     """get the dispersion for every frame to check relative erro by calculate r square score to etimate dispersion"""
     regressions = pn.DataFrame([round(lineraRegressionFrame(x, convertedEgo0),2) for x in np.arange(maxFrames - 1)] ,columns = ['RSqr'])
 
-    """calculate 90 confidence level"""
-    noiseBar = np.percentile(abs(regressions['RSqr']),85);
+    """calculate  bars level"""
+    noiseBar = np.percentile(abs(regressions['RSqr']),90);
+    skipFrameBar = np.percentile(abs(regressions['RSqr']),85);
 
-    skipFrameBar = np.percentile(abs(regressions['RSqr']),5);
     changes=pn.DataFrame()
-    for x in np.arange(1,maxFrames):
+    for frame in np.arange(1,maxFrames):
         """calculate R square and round"""
-        Rsqr = round(lineraRegressionFrame(x,convertedEgo0),2)
+        Rsqr = round(lineraRegressionFrame(frame,convertedEgo0),2)
 
         """if too noisy Rsqr<0.7 use centroids. if no noisy dont correct"""
         if ((abs(Rsqr) < skipFrameBar)):
             d_th = 0;
+            isCentroid = False
+            framePoints = convertedEgo0
+
+            """get the center of gravity to calc movement"""
+            x, y, z = calculateCentroid(frame, framePoints)
         elif (abs(Rsqr) < noiseBar) and (abs(Rsqr) != 1):
             framePoints = Centroids
             isCentroid = True
             """calculate heading using centroids or regular surface"""
-            d_th = calculateHeading(x, framePoints, carLength)
+            d_th = calculateHeading(frame, framePoints, carLength)
+
+            """get the center of gravity to calc movement"""
+            x, y, z = calculateCentroid(frame, framePoints)
         else:
             framePoints = convertedEgo0
             isCentroid = False
             """calculate heading using centroids or regular surface"""
-            d_th = calculateHeading(x,framePoints,carLength)
+            d_th = calculateHeading(frame,framePoints,carLength)
+
+            """get the center of gravity to calc movement"""
+            x, y, z = calculateCentroid(frame, framePoints)
 
         """add to yaw for heading"""
         yaw = yaw+d_th
 
         """calc real heading and error"""
-        RealHeading = calculatYawFromWorld(x)
+        RealHeading = calculatYawFromWorld(frame)
+
+        """get the center of gravity to calc movement"""
+        x, y, z = calculateCentroid(frame, framePoints)
+        dx, dy, dz = [x, y, z] - calculateCentroid(frame - 1, framePoints)
 
         """reset every 5 frames using world"""
-        if (x%5==0):
+        if (frame%5==0):
             yaw=RealHeading
+            x, y, z = calculateCentroid(frame,world)
 
         """calculate error"""
         if (RealHeading!=0):
             error = (yaw-RealHeading)/RealHeading;
 
-        """get the center of gravity to calc movement"""
-        dx,dy,dz = calculateCentroid(x,framePoints)- calculateCentroid(x-1,framePoints)
-        changes = changes.append({'Frame':x,'dx': dx,'dy':dy,'dz': dz,'d_th': d_th,'heading': yaw, 'RealHeading': RealHeading,'error': error, 'centroid use':isCentroid ,'Rsqr': round(Rsqr,2)},True)
+
+        changes = changes.append({'Frame':frame,'dx': dx,'dy':dy,'dz': dz,'d_th': d_th,'heading': yaw, 'RealHeading': RealHeading,'error': error, 'centroid use':isCentroid ,'Rsqr': round(Rsqr,2), 'X':x,'Y':y,'Z':z},True)
+
+changes.to_csv("onlyTargetNoise.csv")
+def printDots(data,frames):
+    a = convertedEgo0.loc[(convertedEgo0['Frame']) <50]
+    ax = plt.axes(projection='3d')
+    ax.scatter3D(a['X'], a['Y'], a['Z'], c=a['Frame'],alpha=0.1)
+    ax.scatter3D(a['X'], a['Y'], a['Z'], color = 'red')
+    convertedEgo0
+    ax.quiver(changes['X'],changes['Y'],changes['Z'],np.sin(np.radians(changes['heading'])),changes['X']*0,np.cos(np.radians(changes['heading'])), color="red",length= 3)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    ax.invert_xaxis()
+    plt.ylim(0,5)
+    ax.cla()
+
+
+changes['error'].mean()
